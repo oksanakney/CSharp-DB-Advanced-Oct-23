27b66a320ca83f29890dfa3f0ab188f49ed600a8
oksanakney <oksanakney@gmail.com>
2023-11-07 16:27:06 +0200

    Exercises for 05.LINQ EFC
    Exercise 3 done, exercise 1 and 2 to be modified and added

---
+++ Exercises/05.LINQ/Data/Models/Album.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models;
+
+public class Album
+{
+    public Album()
+    {
+        this.Songs = new HashSet<Song>();
+    }
+    [Key]
+    public int Id { get; set; }
+    [MaxLength(ValidationConstants.AlbumNameMinLength)]
+    public string Name { get; set; } = null!;
+    public DateTime ReleaseDate { get; set; }
+    [NotMapped]
+    public decimal Price
+        => this.Songs.Sum(s => s.Price);
+    public int? ProducerId { get; set; }
+    [ForeignKey(nameof(ProducerId))]
+    public virtual Producer? Producer { get; set; }
+    public virtual ICollection<Song> Songs { get; set; }
+}
