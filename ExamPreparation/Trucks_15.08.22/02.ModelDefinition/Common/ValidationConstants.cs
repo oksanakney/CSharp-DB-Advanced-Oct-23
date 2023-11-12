4090df09bbeb692f095c11b237187f42a7931967
oksanakney <oksanakney@gmail.com>
2023-11-12 11:22:25 +0200

    Exercise Trucks for Exam Preparation
    02. ModelDefinition

---
+++ ExamPreparation/Trucks_15.08.22/02.ModelDefinition/Common/ValidationConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace Trucks.Common
+{
+    public static class ValidationConstants
+    {
+        //Truck 
+        public const int TruckRegistrationNumberLength = 8;
+        public const int TruckVinNumberMaxLength = 18;
+
+        //Client 
+        public const int ClientNameMaxLength = 40;
+        public const int ClientNationalityMaxLength = 40;
+
+        //Despatcher
+        public const int DespatcherNameMaxLength = 40;
+
+    }
+}
---
+++ ExamPreparation/Trucks_15.08.22/02.ModelDefinition/Data/Models/Enums/CategoryType.cs
@@ -0,0 +1,12 @@
+﻿namespace Trucks.Data.Models.Enums
+{
+    public enum CategoryType
+    {
+        // shtom ne e napisano vzimam defalt-nite stojnosti: 0,1,2,3 kato v masiv
+
+        Flatbed = 0,
+        Jumbo = 1,
+        Refrigerated = 2,
+        Semi = 3,
+    }
+}
