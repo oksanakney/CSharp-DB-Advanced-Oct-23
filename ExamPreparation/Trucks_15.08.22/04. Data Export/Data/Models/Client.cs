d5ddd84d89b327cdc677555e43239f5d83d4e684
oksanakney <oksanakney@gmail.com>
2023-11-26 09:30:55 +0200

    Export Data for Trucks - Exam Preparation
    03.ExportData

---
+++ ExamPreparation/Trucks_15.08.22/04. Data Export/Data/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using Trucks.Common;
+
+namespace Trucks.Data.Models
+{
+    public class Client
+    {
+        public Client()
+        {
+            this.ClientsTrucks = new HashSet<ClientTruck>();
+        }
+
+        [Key]
+        public  int Id { get; set; }
+        [Required]
+        [MaxLength(ValidationConstants.ClientNameMaxLength)]
+        public string Name { get; set; } = null!; // (3 - 40) 3 ne e val nivo baza
+        [Required]
+        [MaxLength(ValidationConstants.ClientNationalityMaxLength)]
+        public string Nationality { get; set; } = null!;
+        [Required]
+        public string Type { get; set; } = null!;
+        public virtual ICollection<ClientTruck> ClientsTrucks { get; set; }
+    }
+}
---
+++ ExamPreparation/Trucks_15.08.22/04. Data Export/Data/Models/Truck.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Trucks.Common;
+using Trucks.Data.Models.Enums;
+
+namespace Trucks.Data.Models;
+
+public class Truck
+{
+    public Truck()
+    {
+        this.ClientsTrucks = new HashSet<ClientTruck>();
+    }
+    [Key]
+    public int Id { get; set; }
+    [MaxLength(ValidationConstants.TruckRegistrationNumberLength)]
+    public string? RegistrationNumber { get; set; } // not required
+    [Required]
+    [MaxLength(ValidationConstants.TruckVinNumberLength)]
+    public string VinNumber { get; set; } = null!;
+    /*in range [950…1420] na nivo baza niama 
+     * nuzhda da pravia check constraint, na nivo importvane da*/
+    public int TankCapacity { get; set; }
+    public int CargoCapacity { get; set;}
+    //Ponezhe tva e enum, to e avtomatichno si required by default
+    //zawoto enum e chislen tip dani
+    public CategoryType CategoryType { get; set; }
+    public MakeType MakeType { get; set; }
+    // int is required by default
+    public int DespatcherId { get; set; }
+    [ForeignKey(nameof(DespatcherId))]
+    public virtual Despatcher Despatcher { get; set; } = null!;
+    public virtual ICollection<ClientTruck> ClientsTrucks { get; set; }
+
+
+}
